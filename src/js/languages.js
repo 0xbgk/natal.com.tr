40e40e67dd40671ce1958a0b22b6c87bd677d8bc
BGK <gorkemkizilok@gmail.com>
2019-04-21 22:19:16 +0300

    added localstorage for multilanguage memory

--- src/js/languages.js
+++ src/js/languages.js
@@ -1,13 +1,28 @@
+var language = localStorage.getItem('language');
+if(language === 'en')
+{
+    $("[data-localize]").localize("application", { language: "en" });    
+}
+else if(language === 'tr')
+{
+    $("[data-localize]").localize("application", { language: "tr" });   
+}
+
 const langENG = document.getElementById('langENG');
 const langTR = document.getElementById('langTR');
 
 langENG.addEventListener('click', (e) => {
     $("[data-localize]").localize("application", { language: "en" });
+
+    localStorage.setItem('language', 'en');
+
     e.preventDefault();
 })
 
 langTR.addEventListener('click', (e) => {
     $("[data-localize]").localize("application", { language: "tr" });   
+    localStorage.setItem('language', 'tr');
+
     e.preventDefault();
 
 })
